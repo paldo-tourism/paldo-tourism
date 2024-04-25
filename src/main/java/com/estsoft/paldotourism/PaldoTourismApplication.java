4c828c4729c3199bc746023631182bac1bce043d
allewis <allewis0201@gmail.com>
2024-04-25 10:53:13 +0900

    스프링부트 기본 설정

---
+++ src/main/java/com/estsoft/paldotourism/PaldoTourismApplication.java
@@ -0,0 +1,13 @@
+package com.estsoft.paldotourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaldoTourismApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaldoTourismApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estsoft/paldotourism/PaldoTourismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estsoft.paldotourism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaldoTourismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
