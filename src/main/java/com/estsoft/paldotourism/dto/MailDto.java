e5e8b8320d49ce4251857a7a8dbd388f71a2005e
dragon0622 <iamdragon.dev@gmail.com>
2024-04-27 21:14:13 +0900

    feat : mail dump 작성 메소드 추가, dto 추가
    mail 내부에 들어가는 내용을 dto를 통해 받아온 값으로 초기화합니다.

--- src/main/java/com/estsoft/paldotourism/dto/MailDto.java
+++ src/main/java/com/estsoft/paldotourism/dto/MailDto.java
@@ -1,5 +1,8 @@
 package com.estsoft.paldotourism.dto;
 
+import com.estsoft.paldotourism.entity.Bus;
+import com.estsoft.paldotourism.entity.Reservation;
+import com.estsoft.paldotourism.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +10,6 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 
 public class MailDto {
     private String dep_terminal;
@@ -17,8 +19,39 @@ public class MailDto {
     private int charge;
     private String bus_grade;
     private String message;
+    private String email;
+    private String nickname;
+    private String reservationNumber;
 
     //아래 항목 테스트 변수로 삭제 예정입니다:)
     private String address;
     private String title;
+
+//    public MailDto(Bus reservationBus) {
+//        this.dep_terminal = reservationBus.dep_terminal;
+//        this.arr_terminal = reservationBus.arr_terminal;
+//        this.dep_time = reservationBus.dep_time;
+//        this.arr_time = reservationBus.arr_time;
+//        this.charge = reservationBus.charge;
+//        this.bus_grade = reservationBus.busgrade;
+//    }
+//    public MailDto(Reservation reservation) {
+//        this.reservationNumber = reservation.reservationNumber;
+//    }
+//    public MailDto(User user) { //매개변수 변경이 필요합니다.
+//        this.email = user.getEmail();
+//    }
+
+    public String makeMessageDump(){
+        this.message = this.nickname + "님의 예약 내역입니다."
+                + dep_terminal + "터미널 에서 "
+                + dep_time + "에 출발하여"
+                + arr_terminal + "터미널에 "
+                + arr_time + "에 도착하는 "
+                + bus_grade + "등급 버스를 예매 하셨습니다."
+                + charge + "원 결제 하셨으며,"
+                + "예약번호는 " + reservationNumber + "입니다.";
+
+        return message;
+    }
 }
