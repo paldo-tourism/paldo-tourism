0e52635191c0e996a942d79d61a941f405879f0c
dragon0622 <iamdragon.dev@gmail.com>
2024-04-30 17:04:22 +0900

    refactor : mailDto 수정
    프로젝트 의도에 맞게 동작하기위해 dto를 수정했습니다.
    set의 경우 mailService 에서 진행했습니다.

--- src/main/java/com/estsoft/paldotourism/dto/MailDto.java
+++ src/main/java/com/estsoft/paldotourism/dto/MailDto.java
@@ -1,16 +1,10 @@
 package com.estsoft.paldotourism.dto;
 
-import com.estsoft.paldotourism.entity.Bus;
-import com.estsoft.paldotourism.entity.Reservation;
-import com.estsoft.paldotourism.entity.User;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
-
+@Builder
 public class MailDto {
     private String dep_terminal;
     private String arr_terminal;
@@ -22,36 +16,21 @@ public class MailDto {
     private String email;
     private String nickname;
     private String reservationNumber;
+    private String seatNumber;
 
-    //아래 항목 테스트 변수로 삭제 예정입니다:)
-    private String address;
-    private String title;
-
-//    public MailDto(Bus reservationBus) {
-//        this.dep_terminal = reservationBus.dep_terminal;
-//        this.arr_terminal = reservationBus.arr_terminal;
-//        this.dep_time = reservationBus.dep_time;
-//        this.arr_time = reservationBus.arr_time;
-//        this.charge = reservationBus.charge;
-//        this.bus_grade = reservationBus.busgrade;
-//    }
-//    public MailDto(Reservation reservation) {
-//        this.reservationNumber = reservation.reservationNumber;
-//    }
-//    public MailDto(User user) { //매개변수 변경이 필요합니다.
-//        this.email = user.getEmail();
-//    }
 
     public String makeMessageDump(){
-        this.message = this.nickname + "님의 예약 내역입니다."
-                + dep_terminal + "터미널 에서 "
-                + dep_time + "에 출발하여"
-                + arr_terminal + "터미널에 "
-                + arr_time + "에 도착하는 "
-                + bus_grade + "등급 버스를 예매 하셨습니다."
-                + charge + "원 결제 하셨으며,"
-                + "예약번호는 " + reservationNumber + "입니다.";
+        StringBuilder messageBuilder = new StringBuilder();
+        messageBuilder.append(this.nickname)
+                .append("님의 예약 내역입니다.")
+                .append(dep_terminal).append("터미널에서 ")
+                .append(dep_time).append("에 출발하여 ")
+                .append(arr_terminal).append("터미널에 ")
+                .append(arr_time).append("에 도착하는 ")
+                .append(bus_grade).append("등급 버스를 예매 하셨습니다. ")
+                .append(charge).append("원 결제 하셨으며, ")
+                .append("예약번호는 ").append(reservationNumber).append("입니다.");
 
-        return message;
+        return this.message = messageBuilder.toString();
     }
 }
