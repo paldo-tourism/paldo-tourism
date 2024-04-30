dd6616e1683c18d3f73fe413cd19f94c6be00e97
kanghanju <hanju1017@gmail.com>
2024-04-30 16:05:49 +0900

    feat: 메인화면과 timeTable에 데이터를 전달해줄 Dto 생성

---
+++ src/main/java/com/estsoft/paldotourism/dto/bus/BusInfoFindRequestDto.java
@@ -0,0 +1,25 @@
+package com.estsoft.paldotourism.dto.bus;
+
+import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class BusInfoFindRequestDto {
+
+    //TODO validator를 이용한 유효성 검사
+    @NotBlank(message = "출발지 입력은 필수입니다.")
+    private String depTerminalName;
+
+    @NotBlank(message = "도착지 입력은 필수입니다.")
+    private String arrTerminalName;
+
+    @NotBlank(message = "출발 날짜 입력은 필수입니다.")
+    private String depDate;
+
+    @NotBlank(message = "버스 등급 입력은 필수입니다.")
+    private String busGrade;
+
+}
