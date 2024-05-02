72c6f1959e7c621291ddabf208f518e24ca76dd8
kanghanju <66769475+kanghanju@users.noreply.github.com>
2024-05-02 17:23:40 +0900

    [ALL] 메인페이지 화면 제작 (#42)
    * feat: 메인화면 html/css 생성
    
    * chore: 필요없는 Transactional어노테이션 삭제 및 public메서드를 private로 변경
    
    * chore: Status ENUM 삭제
    
    * feat: 메인페이지 터미널 search,지역별 search,조회하기 버튼 로직 추가
    
    * chore: timeTable 타임리프 문법 수정
    
    * feat: 메인화면 출발지,도착지 모달 추가
    
    * chore: 필요없는 주석 삭제
    
    * chore: 메인화면 js 로직 (조회하기 버튼,날짜 선택)
    
    * chore: 출발지 선택을 a태그에서 input으로 변경(사용자가 출/도착지 선택을 안했을시 빈값으로 입력받기위함)

--- src/main/java/com/estsoft/paldotourism/entity/Status.java
+++
@@ -1,6 +0,0 @@
-package com.estsoft.paldotourism.entity;
-
-public enum Status {
-
-    STATUS_NORESERVATION,STATUS_RESERVATIONING,STATUS_RESERVATION
-}
--- src/main/java/com/estsoft/paldotourism/repository/BusTerminalRepository.java
+++ src/main/java/com/estsoft/paldotourism/repository/BusTerminalRepository.java
@@ -1,6 +1,7 @@
 package com.estsoft.paldotourism.repository;
 
 import com.estsoft.paldotourism.entity.BusTerminal;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,8 @@ import org.springframework.stereotype.Repository;
 public interface BusTerminalRepository extends JpaRepository<BusTerminal,String> {
 
     Optional<BusTerminal> findByName(String terminalName);
+
+    List<BusTerminal> findByRegion(String region);
+
+    List<BusTerminal> findByNameContaining(String terminalName);
 }
