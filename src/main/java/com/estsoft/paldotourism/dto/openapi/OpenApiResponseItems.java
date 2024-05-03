bae2a5e111c7405036609b841f51e97db7f735f2
kanghanju <hanju1017@gmail.com>
2024-05-03 15:49:47 +0900

    fix: JSON 파싱 문제 해결

--- src/main/java/com/estsoft/paldotourism/dto/openapi/OpenApiResponseItems.java
+++ src/main/java/com/estsoft/paldotourism/dto/openapi/OpenApiResponseItems.java
@@ -1,5 +1,6 @@
 package com.estsoft.paldotourism.dto.openapi;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.List;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,5 +10,6 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class OpenApiResponseItems {
+    @JsonFormat(with = JsonFormat.Feature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
     private List<OpenApiResponseBusItem> item;
 }
