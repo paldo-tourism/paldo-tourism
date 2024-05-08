66b9c67298fa2b7e085e785dcfce6ee25ee6795e
dragon0622 <iamdragon.dev@gmail.com>
2024-05-08 16:36:37 +0900

    feat: 컨트롤러 생성 및 메소드 구현
    폼에서 버튼을 누르면 alanService의 askAi메소드를 실행합니다.

---
+++ src/main/java/com/estsoft/paldotourism/controller/AlanController.java
@@ -0,0 +1,25 @@
+package com.estsoft.paldotourism.controller;
+
+import com.estsoft.paldotourism.dto.AlanDto;
+import com.estsoft.paldotourism.service.AlanService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+
+
+@Controller
+public class AlanController {
+    private final AlanService alanService;
+
+    public AlanController(AlanService alanService) {
+        this.alanService = alanService;
+    }
+
+    @PostMapping("/api/ai/aiRecommend")
+    public String processAlanForm(@ModelAttribute AlanDto alanDto, Model model){
+        String aiResponse = alanService.askAi(alanDto);
+
+        model.addAttribute("response",aiResponse);
+        return "/ai/test";
+    }
+}
