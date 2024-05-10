24e22c009d6e2f4f548f076b76914b717f18ac30
Allewis <133013702+Allewis0201@users.noreply.github.com>
2024-05-10 15:18:02 +0900

    [ALL] 좌석 테이블 예약 테이블 연결 (#91)
    * refactor: html 타이틀 변경
    
    타이틀 변경
    
    * feat: html 타이틀 변경 및 링크 추가
    
    타이틀 변경 및 메인으로 향하는 링크 추가
    
    * refactor: 리다이렉트 함수 위치 변경
    
    메일 함수 결과 안에 리다이렉트 함수를 호출하게끔 함수 위치 변경
    
    * feat: 결제 후 예약 완료 설정
    
    결제를 완료한 후 좌석들을 예약 완료 상태로 변경하는 로직 추가함
    
    * feat: 스케줄링 및 예약 변경/취소 수정
    
    예약 변경/취소 때 좌석 정보를 불러오는 함수를 findAllByBus가 아닌 findAllByReservation으로 수정
    
    예약 변경/취소 때 좌석 테이블에 연결된 예약 테이블 외래키를 null로 수정하는 로직 추가함
    
    스케줄링 함수에서 예약 외래키가 null이 아닌 좌석들을 처리하는 것으로 로직을 수정함, 그 후에 좌석의 예약 외래키를 null로 수정

--- src/main/java/com/estsoft/paldotourism/service/ReservationService.java
+++ src/main/java/com/estsoft/paldotourism/service/ReservationService.java
@@ -64,7 +64,7 @@ public class ReservationService {
         Reservation saveReservation = reservationRepository.save(reservation);
 
 
-        // 선택한 좌석들을 가져와 상태를 선택중으로 변경
+        // 선택한 좌석들을 가져와 상태를 선택중으로 변경 및 좌석들에 예약 외래키 연결
         for (int i = 0; i < requestDto.getSeatNumbers().size(); i++) {
             Integer seatNumber = requestDto.getSeatNumbers().get(i);
 
@@ -116,13 +116,14 @@ public class ReservationService {
         Reservation reservation = reservationRepository.findById(reservationId).get();
         reservation.updateReservationStatus(Status.STATUS_CANCEL);
 
-        // 기존 예약의 좌석 상태를 빈 자리로 변경
+        // 기존 예약의 좌석 상태를 빈 자리로 변경 및 좌석 테이블의 예약 외래키 해제
         Bus reservationedBus = reservation.getBus();
-        List<Seat> seatList = seatRepository.findAllByBus(reservationedBus);
+        List<Seat> seatList = seatRepository.findAllByReservation(reservation);
 
         for (Seat seat : seatList) {
             Integer seatNumber = seat.getSeatNumber();
             updateSeatReservationStatus(reservationedBus, seatNumber, SeatStatus.EMPTY);
+            updateSeatReservation(null, reservationedBus, seatNumber);
         }
 
         //결제 상태 취소로 변경
@@ -171,14 +172,15 @@ public class ReservationService {
         reservation.updateReservationStatus(Status.STATUS_CANCEL);
 
 
-        // 예약의 좌석 상태를 빈 자리로 변경
+        // 예약의 좌석 상태를 빈 자리로 변경 및 좌석과 연결된 예약 외래키 해제
         Bus reservationedBus = reservation.getBus();
-        List<Seat> seatList = seatRepository.findAllByBus(reservationedBus);
+        List<Seat> seatList = seatRepository.findAllByReservation(reservation);
 
 
         for (Seat seat : seatList) {
             Integer seatNumber = seat.getSeatNumber();
             updateSeatReservationStatus(reservationedBus, seatNumber, SeatStatus.EMPTY);
+            updateSeatReservation(null, reservationedBus, seatNumber);
         }
 
         //결제 상태 취소로 변경
@@ -303,9 +305,14 @@ public class ReservationService {
 
         seatList.forEach(seat -> {
             if (seat.getModifiedDateTime().plusMinutes(10).isBefore(now)) {
-                Reservation reservation = seat.getReservation();
-                reservationList.add(reservation);
-                seat.updateStatus(SeatStatus.EMPTY);
+
+                if(seat.getReservation() != null)
+                {
+                    Reservation reservation = seat.getReservation();
+                    reservationList.add(reservation);
+                    seat.updateStatus(SeatStatus.EMPTY);
+                    seat.updateReservation(null);
+                }
             }
         });
 
