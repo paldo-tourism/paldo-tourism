5294723f91b2b83f593e7dbee025f1ad76b97999
kanghanju <hanju1017@gmail.com>
2024-05-10 15:09:39 +0900

    feat: invalid한 파라미터 예외 추가

--- src/main/java/com/estsoft/paldotourism/exception/GlobalExceptionHandler.java
+++ src/main/java/com/estsoft/paldotourism/exception/GlobalExceptionHandler.java
@@ -6,6 +6,9 @@ import com.estsoft.paldotourism.exception.likes.AlreadyLikedException;
 import com.estsoft.paldotourism.exception.likes.NotLikedYetException;
 import com.estsoft.paldotourism.exception.openapi.BusRouteNotFoundException;
 import com.estsoft.paldotourism.exception.openapi.UrlNotValidException;
+import com.estsoft.paldotourism.exception.request.InvalidBusGradeRequestException;
+import com.estsoft.paldotourism.exception.request.InvalidDateRequestException;
+import com.estsoft.paldotourism.exception.reservation.ReservationNotAllowedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -59,6 +62,29 @@ public class GlobalExceptionHandler {
         modelAndView.addObject("message", "존재하지 않는 버스입니다.");
         modelAndView.setStatus(HttpStatus.NOT_FOUND);
         return modelAndView;
+    }
+
+    //timeTable request 관련 에러
+    @ExceptionHandler(InvalidDateRequestException.class)
+    public String InvalidDateRequestException(InvalidDateRequestException e, RedirectAttributes redirectAttributes) {
+        ErrorForm errorForm = new ErrorForm(HttpStatus.BAD_REQUEST.value(),"날짜는 'yyyyMMdd' 형식을 사용해야 합니다. 또한 출발 날짜는 오늘부터 모레까지의 날짜만 가능합니다.");
+        redirectAttributes.addFlashAttribute("error",errorForm);
+        //이렇게 하면 404페이지가 뜨긴하는데 ... status가 200 OK로 뜬다..
+        return "redirect:/404";
+    }
+
+    @ExceptionHandler(InvalidBusGradeRequestException.class)
+    public String InvalidBusGradeRequestException(InvalidBusGradeRequestException e, RedirectAttributes redirectAttributes) {
+        ErrorForm errorForm = new ErrorForm(HttpStatus.BAD_REQUEST.value(),"버스 등급은 '우등' 또는 '일반'만 가능합니다.");
+        redirectAttributes.addFlashAttribute("error",errorForm);
+        return "redirect:/404";
+    }
+
+    //reservation 관련 에러
+    @ExceptionHandler(ReservationNotAllowedException.class)
+    public ResponseEntity<ErrorForm> ReservationNotAllowedException(ReservationNotAllowedException e) {
+        ErrorForm errorForm = new ErrorForm(HttpStatus.BAD_REQUEST.value(), "해당 버스는 예약불가능합니다.");
+        return new ResponseEntity<>(errorForm,HttpStatus.BAD_REQUEST);
     }
 
 }
