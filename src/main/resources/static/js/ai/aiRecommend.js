da0cb62e2b3d497130a842bc5a6f8e80539a1e2c
kanghanju <hanju1017@gmail.com>
2024-05-13 15:17:21 +0900

    feat: 로딩화면 추가

--- src/main/resources/static/js/ai/aiRecommend.js
+++ src/main/resources/static/js/ai/aiRecommend.js
@@ -14,14 +14,23 @@ window.onload = function() {
     document.getElementById('travelDate').setAttribute('min', getCurrentDate());
 };
 
-document.addEventListener("DOMContentLoaded", function() {
-    var form = document.getElementById("form");
-    form.addEventListener("submit", function(event) {
-        var travelDate = document.getElementById("travelDate").value;
+document.getElementById('form').addEventListener('submit', function (event) {
+    var travelDate = document.getElementById("travelDate").value;
 
-        if (travelDate === "") {
-            event.preventDefault(); // 제출 중단
-            alert("여행 날짜를 입력해주세요.");
-        }
-    });
+    if (travelDate === "") {
+        event.preventDefault(); // 제출 중단
+        alert("여행 날짜를 입력해주세요.");
+        return;
+    }
+
+    // 기본 폼 제출 동작을 중단
+    event.preventDefault();
+
+    // 로딩 화면 표시
+    document.getElementById('loading').style.display = 'block';
+
+    // 폼 데이터를 실제로 제출
+    this.submit();
 });
+
+
